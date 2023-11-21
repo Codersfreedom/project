ce2ab68a43d9e1719516d6e868025a06d66eca4f
BelugaBoy <rakeshmanna762@gmail,com>
2023-11-21 10:33:17 +0530

    added footer

--- index.php
+++ index.php
@@ -1,24 +1,29 @@
 <!doctype html>
 <html lang="en">
-  <head>
-    <!-- Required meta tags -->
-    <meta charset="utf-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
 
-    <!-- Bootstrap CSS -->
-    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.0.0/dist/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
-    <link rel="stylesheet" href="\project\assets\css\style.css">
-<!-- favicon -->
-<link rel="icon" type="image/png" sizes="16x16" href="./assets/images/favicon.png" />
+<head>
+  <!-- Required meta tags -->
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
 
-    <title>NITMAS-TTMS PROJECT</title>
-  </head>
-  <body>
-<?php include 'partials/_header.php'; ?>
+  <!-- Bootstrap CSS -->
+  <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.0.0/dist/css/bootstrap.min.css"
+    integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
+  <link rel="stylesheet" href="\project\assets\css\style.css">
+  <!-- Font Awesome -->
+  <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+  <!-- favicon -->
+  <link rel="icon" type="image/png" sizes="16x16" href="./assets/images/favicon.png" />
 
-<!-- //carosusel -->
+  <title>NITMAS-TTMS PROJECT</title>
+</head>
 
-<div id="carouselExampleIndicators" class="carousel slide" data-ride="carousel">
+<body>
+  <?php include 'partials/_header.php'; ?>
+
+  <!-- //carosusel -->
+
+  <div id="carouselExampleIndicators" class="carousel slide" data-ride="carousel">
     <ol class="carousel-indicators">
       <li data-target="#carouselExampleIndicators" data-slide-to="0" class="active"></li>
       <li data-target="#carouselExampleIndicators" data-slide-to="1"></li>
@@ -28,19 +33,22 @@
     </ol>
     <div class="carousel-inner">
       <div class="carousel-item active">
-        <img class="d-block w-100" style="width:2400px; height: 600px; " src="assets/img/Nitmas_banner.jpg" alt="First slide">
+        <img class="d-block w-100" style="width:2400px; height: 600px; " src="assets/img/Nitmas_banner.jpg"
+          alt="First slide">
       </div>
       <div class="carousel-item">
-        <img class="d-block w-100" style="width:2400px; height: 600px; " src="assets/img/Banner2.jpg" alt="Second slide">
+        <img class="d-block w-100" style="width:2400px; height: 600px; " src="assets/img/Banner2.jpg"
+          alt="Second slide">
       </div>
       <div class="carousel-item">
-        <img class="d-block w-100"  style="width:2400px; height: 600px; "src="assets/img/Banner3.jpg" alt="Third slide">
+        <img class="d-block w-100" style="width:2400px; height: 600px; " src="assets/img/Banner3.jpg" alt="Third slide">
       </div>
       <div class="carousel-item">
-        <img class="d-block w-100"  style="width:2400px; height: 600px; "src="assets/img/Library.jpg" alt="Third slide">
+        <img class="d-block w-100" style="width:2400px; height: 600px; " src="assets/img/Library.jpg" alt="Third slide">
       </div>
       <div class="carousel-item">
-        <img class="d-block w-100"  style="width:2400px; height: 600px; "src="assets/img/language_Laboratory.jpg" alt="Third slide">
+        <img class="d-block w-100" style="width:2400px; height: 600px; " src="assets/img/language_Laboratory.jpg"
+          alt="Third slide">
       </div>
     </div>
     <a class="carousel-control-prev" href="#carouselExampleIndicators" role="button" data-slide="prev">
@@ -53,42 +61,47 @@
     </a>
   </div>
 
-<!-- Faculty section -->
-<h2 class ="text-center my-3">Our Faculty</h2>
-<hr>
- <div class="container d-flex flex-wrap justify-content-between my-3">
+  <!-- Faculty section -->
+  <h2 class="text-center my-3">Our Faculty</h2>
+  <hr>
+  <div class="container d-flex flex-wrap justify-content-between my-3">
 
- <div class="card" style="width: 18rem;">
-  <img class="card-img-top" src="assets\img\faculty\Principal.png" alt="Card image cap">
-  <div class="card-body">
-    <h5 class="card-title">Prof. Sujoy Biswas</h5>
-    <h6>Principal</h6>
-    <p class="card-text" >Some quick example text to build on the card title and make up the bulk of the card's content.</p>
-    <a href="#" class="btn btn-primary">Learn more</a>
-  </div>
-</div>
+    <div class="card" style="width: 18rem;">
+      <img class="card-img-top" src="assets\img\faculty\Principal.png" alt="Card image cap">
+      <div class="card-body">
+        <h5 class="card-title">Prof. Sujoy Biswas</h5>
+        <h6>Principal</h6>
+        <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's
+          content.</p>
+        <a href="#" class="btn btn-primary">Learn more</a>
+      </div>
+    </div>
 
-<div class="card"  style="width: 18rem;">
-  <img class="card-img-top" style="width:286.4px; height: 286.4px; " src="assets\img\faculty\SS.jpg" alt="Card image cap">
-  <div class="card-body">
-    <h5 class="card-title">Prof. Sunanda Sinha</h5>
-    <h6>Assistant Professor</h6>
-    <p class="card-text">E-Commerce, Database Management System, Software Engineering, Object Oriented Programming, Introduction to Computing.</p>
-    <a href="#" class="btn btn-primary">Learn more</a>
-  </div>
-</div>
+    <div class="card" style="width: 18rem;">
+      <img class="card-img-top" style="width:286.4px; height: 286.4px; " src="assets\img\faculty\SS.jpg"
+        alt="Card image cap">
+      <div class="card-body">
+        <h5 class="card-title">Prof. Sunanda Sinha</h5>
+        <h6>Assistant Professor</h6>
+        <p class="card-text">E-Commerce, Database Management System, Software Engineering, Object Oriented Programming,
+          Introduction to Computing.</p>
+        <a href="#" class="btn btn-primary">Learn more</a>
+      </div>
+    </div>
 
-<div class="card" style="width: 18rem;">
-  <img class="card-img-top " style="width:286.4px; height: 286.4px; " src="assets\img\faculty\SB.jpg" alt="Card image cap">
-  <div class="card-body">
-    <h5 class="card-title">Prof. Souvik Biswas</h5>
-    <h6>Assistant Professor</h6>
-    <p class="card-text">Introduction to Computing, Compiler Design, Numerical Methods, Data Structure, Design and Analysis of Algorithm, E-Commerce.</p>
-    <a href="#" class="btn btn-primary">Learn more</a>
-  </div>
-</div>
+    <div class="card" style="width: 18rem;">
+      <img class="card-img-top " style="width:286.4px; height: 286.4px; " src="assets\img\faculty\SB.jpg"
+        alt="Card image cap">
+      <div class="card-body">
+        <h5 class="card-title">Prof. Souvik Biswas</h5>
+        <h6>Assistant Professor</h6>
+        <p class="card-text">Introduction to Computing, Compiler Design, Numerical Methods, Data Structure, Design and
+          Analysis of Algorithm, E-Commerce.</p>
+        <a href="#" class="btn btn-primary">Learn more</a>
+      </div>
+    </div>
 
-<!-- <div class="card" style="width: 18rem;">
+    <!-- <div class="card" style="width: 18rem;">
   <img class="card-img-top" style="width:286.4px; height: 286.4px; " src="assets\img\faculty\DSD.jpg" alt="Card image cap">
   <div class="card-body">
     <h5 class="card-title">Prof. Deep Suman Dev</h5>
@@ -99,13 +112,13 @@
 </div> -->
 
 
- </div>
+  </div>
 
-<!--  Faculty Section ends here -->
+  <!--  Faculty Section ends here -->
 
-<!-- Contact section -->
+  <!-- Contact section -->
 
-<div class="container">
+  <!-- <div class="container">
     <div class="row set-row-pad">
         <div class="col-lg-4 col-md-4 col-sm-4   col-lg-offset-1 col-md-offset-1 col-sm-offset-1 "
              data-scroll-reveal="enter from the bottom after 0.4s">
@@ -130,29 +143,136 @@
             <hr/>
             <div>
                 <a href="#"> <img src="assets/img/Social/facebook.png" alt=""/> </a>
-                <!-- <a href="#"> <img src="assets/img/Social/google-plus.png" alt=""/></a> -->
-                <a href="#"> <img src="assets/img/Social/twitter.png" alt=""/></a>
+                 <a href="#"> <img src="assets/img/Social/google-plus.png" alt=""/></a> -->
+  <!-- <a href="#"> <img src="assets/img/Social/twitter.png" alt=""/></a>
             </div>
         </div>
 
 
     </div>
-</div>
+</div>  -->
 
 
-<!-- CONTACT SECTION END-->
-<!-- <div id="footer">
+  <!-- Footer -->
+  <footer class="text-center text-lg-start text-white" style="background-color: #929fba">
+    <!-- Grid container -->
+    <div class="container p-4 pb-0">
+      <!-- Section: Links -->
+      <section class="">
+        <!--Grid row-->
+        <div class="row">
+          <!-- Grid column -->
+          <div class="col-md-3 col-lg-3 col-xl-3 mx-auto mt-3">
+            <h6 class="text-uppercase mb-4 font-weight-bold">
+              Routine Generator
+            </h6>
+            <p>
+              This Website provides a better way of generating routine for a
+              university with a optimize and dynamic way.
+            </p>
+          </div>
+          <!-- Grid column -->
+
+          <hr class="w-100 clearfix d-md-none" />
+
+          <!-- Grid column -->
+          <div class="col-md-2 col-lg-2 col-xl-2 mx-auto mt-3">
+            <h6 class="text-uppercase mb-4 font-weight-bold">Features</h6>
+            <p>
+              <a class="text-white">Manage Faculty</a>
+            </p>
+            <p>
+              <a class="text-white">Manage Subjects</a>
+            </p>
+            <p>
+              <a class="text-white">Check Status</a>
+            </p>
+            <p>
+              <a class="text-white">Generate Routine</a>
+            </p>
+          </div>
+          <!-- Grid column -->
+
+          <hr class="w-100 clearfix d-md-none" />
+
+          <!-- Grid column -->
+          <hr class="w-100 clearfix d-md-none" />
+
+          <!-- Grid column -->
+          <div class="col-md-4 col-lg-3 col-xl-3 mx-auto mt-3">
+            <h6 class="text-uppercase mb-4 font-weight-bold">Contact</h6>
+            <p><i class="fas fa-home mr-3"></i> Jhinga, Diamond Harbour Road</p>
+            <p><i class="fas fa-envelope mr-3"></i> P.O. Amira, South 24 Parganas</p>
+            <p><i class="fas fa-phone mr-3"></i> info@nitmas.edu.in</p>
+            <p><i class="fas fa-print mr-3"></i> 03174 -245078/101</p>
+          </div>
+          <!-- Grid column -->
+
+          <!-- Grid column -->
+          <div class="col-md-3 col-lg-2 col-xl-2 mx-auto mt-3">
+            <h6 class="text-uppercase mb-4 font-weight-bold">Follow us</h6>
+
+            <!-- Facebook -->
+            <a class="btn btn-primary btn-floating m-1" style="background-color: #3b5998" href="#!" role="button"><i
+                class="fab fa-facebook-f"></i></a>
+
+            <!-- Twitter -->
+            <a class="btn btn-primary btn-floating m-1" style="background-color: #55acee" href="#!" role="button"><i
+                class="fab fa-twitter"></i></a>
+
+            <!-- Google -->
+            <a class="btn btn-primary btn-floating m-1" style="background-color: #dd4b39" href="#!" role="button"><i
+                class="fab fa-google"></i></a>
+
+            <!-- Instagram -->
+            <a class="btn btn-primary btn-floating m-1" style="background-color: #ac2bac" href="#!" role="button"><i
+                class="fab fa-instagram"></i></a>
+
+            <!-- Linkedin -->
+            <a class="btn btn-primary btn-floating m-1" style="background-color: #0082ca" href="#!" role="button"><i
+                class="fab fa-linkedin-in"></i></a>
+            <!-- Github -->
+            <a class="btn btn-primary btn-floating m-1" style="background-color: #333333" href="#!" role="button"><i
+                class="fab fa-github"></i></a>
+          </div>
+        </div>
+        <!--Grid row-->
+      </section>
+      <!-- Section: Links -->
+    </div>
+    <!-- Grid container -->
+
+    <!-- Copyright -->
+    <div class="text-center p-3" style="background-color: rgba(0, 0, 0, 0.2)">
+      © 2023 Copyright:
+      <a class="text-white" href="#">Routine Generator</a>
+    </div>
+    <!-- Copyright -->
+  </footer>
+  <!-- Footer -->
+
+
+
+  <!-- CONTACT SECTION END-->
+  <!-- <div id="footer">
       &copy 2023 yourdomain.com | All Rights Reserved |  <a href="http://binarytheme.com" style="color: #fff" target="_blank">Design by : binarytheme.com</a>
 </div> -->
 
 
 
-  
 
-    <!-- Optional JavaScript -->
-    <!-- jQuery first, then Popper.js, then Bootstrap JS -->
-    <script src="https://code.jquery.com/jquery-3.2.1.slim.min.js" integrity="sha384-KJ3o2DKtIkvYIK3UENzmM7KCkRr/rE9/Qpg6aAZGJwFDMVNA/GpGFF93hXpG5KkN" crossorigin="anonymous"></script>
-    <script src="https://cdn.jsdelivr.net/npm/popper.js@1.12.9/dist/umd/popper.min.js" integrity="sha384-ApNbgh9B+Y1QKtv3Rn7W3mgPxhU9K/ScQsAP7hUibX39j7fakFPskvXusvfa0b4Q" crossorigin="anonymous"></script>
-    <script src="https://cdn.jsdelivr.net/npm/bootstrap@4.0.0/dist/js/bootstrap.min.js" integrity="sha384-JZR6Spejh4U02d8jOt6vLEHfe/JQGiRRSQQxSfFWpi1MquVdAyjUar5+76PVCmYl" crossorigin="anonymous"></script>
-  </body>
+
+  <!-- Optional JavaScript -->
+  <!-- jQuery first, then Popper.js, then Bootstrap JS -->
+  <script src="https://code.jquery.com/jquery-3.2.1.slim.min.js"
+    integrity="sha384-KJ3o2DKtIkvYIK3UENzmM7KCkRr/rE9/Qpg6aAZGJwFDMVNA/GpGFF93hXpG5KkN"
+    crossorigin="anonymous"></script>
+  <script src="https://cdn.jsdelivr.net/npm/popper.js@1.12.9/dist/umd/popper.min.js"
+    integrity="sha384-ApNbgh9B+Y1QKtv3Rn7W3mgPxhU9K/ScQsAP7hUibX39j7fakFPskvXusvfa0b4Q"
+    crossorigin="anonymous"></script>
+  <script src="https://cdn.jsdelivr.net/npm/bootstrap@4.0.0/dist/js/bootstrap.min.js"
+    integrity="sha384-JZR6Spejh4U02d8jOt6vLEHfe/JQGiRRSQQxSfFWpi1MquVdAyjUar5+76PVCmYl"
+    crossorigin="anonymous"></script>
+</body>
+
 </html>
